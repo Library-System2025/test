1749f014165a20c89da5926cc68da5f7199110b9
USER <USER@192.168.2.198>
2025-10-04 22:12:15 +0300

    test test22222

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,6 +2,7 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("Hello my best partnrt ever never");
+        System.out.println("Hello manar");
     }
 
 }
