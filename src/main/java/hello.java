960035237259b7b838eb29a45521a64e893bc094
Manar Nofal <manarnofal01@gmail.com>
2025-10-05 00:02:03 +0300

    errrrrrrrrror

--- src/main/java/hello.java
+++
@@ -1,9 +0,0 @@
-public class hello {
-
-    public static void main(String[] args) {
-        System.out.println("Hello my best partnrt ever never");
-        System.out.println("Hello manar");
-        System.out.println("Hello zezooooooo");
-    }
-
-}
---
+++ src/main/java/src/test/java/Testhello.java
@@ -0,0 +1,43 @@
+package src.test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hello;
+
+class Testhello {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testadd() 
+	{
+int i1=2;
+int i2=3;
+int er=5;
+int ar = hello.add(i1, i2);
+
+assertTrue(er==ar);
+
+	}
+
+}
