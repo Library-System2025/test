4f25e63a32b190399344a3cd17896aa5499943fb
Manar Nofal <manarnofal01@gmail.com>
2025-10-04 21:37:09 +0300

    Manar edit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,9 +1,7 @@
-
 public class hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello my best partnrt ever never");
+    }
 
 }
