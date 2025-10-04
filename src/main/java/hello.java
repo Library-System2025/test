91ff41f628453420353ec86fa5fe71f6978f4151
Manar Nofal <manarnofal01@gmail.com>
2025-10-04 22:15:48 +0300

    manar2

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,6 +3,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello my best partnrt ever never");
         System.out.println("Hello manar");
+        System.out.println("Hello zezooooooo");
     }
 
 }
