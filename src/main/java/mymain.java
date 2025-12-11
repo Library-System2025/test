e0569a6ac14dffcebf86b085387981a98bb5771b
USER <USER@192.168.2.194>
2025-12-11 22:42:36 +0200

    for delivery(final)

--- src/main/java/mymain.java
+++ src/main/java/mymain.java
@@ -1,44 +1,44 @@
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-///**
-// * Main entry point for the Library Application.
-// *
-// * @author Zainab
-// * @version 1.0
-// */
-//
-//public class mymain extends Application {
-//
-//	
-//    
-//    /**
-//     * @param primaryStage 
-//     * @throws Exception 
-//     */
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//        
-//        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml")); 
-//
-//        
-//        Scene scene = new Scene(root);
-//
-//        
-//        primaryStage.setTitle("Library Management - Login");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-//
-//    /** 
-//     * @param args 
-//     */
-//    
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//    }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+* Main entry point for the Library Application.
+ *
+ * @author Zainab
+ * @version 1.0
+ */
+
+public class mymain extends Application {
+
+	
+    
+  /**
+   * @param primaryStage 
+     * @throws Exception 
+    */
+
+   @Override
+   public void start(Stage primaryStage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml")); 
+
+        
+       Scene scene = new Scene(root);
+
+        
+        primaryStage.setTitle("Library Management - Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+   /** 
+     * @param args 
+     */
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    }
