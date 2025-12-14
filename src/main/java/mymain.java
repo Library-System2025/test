effba4398e72ef05959cb6d141e769f19c858b4c
Manar Nofal <manarnofal01@gmail.com>
2025-12-14 13:40:48 +0200

    rr

--- src/main/java/mymain.java
+++ src/main/java/mymain.java
@@ -5,73 +5,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
-<<<<<<< HEAD
  * Main entry point for the Library Application.
  *
  * @author Zainab
  * @version 1.0
  */
-
 public class mymain extends Application {
-
-	
-    
-    /**
-     * @param primaryStage 
-     * @throws Exception 
-     */
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
         Parent root = FXMLLoader.load(getClass().getResource("/login.fxml")); 
-
-        
         Scene scene = new Scene(root);
 
-        
         primaryStage.setTitle("Library Management - Login");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /** 
-=======
-* Main entry point for the Library Application.
- *
- * @author Zainab
- * @version 1.0
- */
-
-public class mymain extends Application {
-
-	
-    
-  /**
-   * @param primaryStage 
-     * @throws Exception 
-    */
-
-   @Override
-   public void start(Stage primaryStage) throws Exception {
-        
-        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml")); 
-
-        
-       Scene scene = new Scene(root);
-
-        
-        primaryStage.setTitle("Library Management - Login");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-   /** 
->>>>>>> branch 'main' of https://github.com/zainab-hashem/test.git
-     * @param args 
-     */
-    
     public static void main(String[] args) {
         launch(args);
     }
-    }
+}
